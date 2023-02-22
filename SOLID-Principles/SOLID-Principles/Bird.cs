2ab4bbc58531ee942a60c9c4cff1ae0ed7ba1ac2
Tommy Persaud <tpersaud27@gmail.com>
2023-02-22 10:33:03 -0500

    Liskov Substitution Principle Example
    Demonstrated the implementation that breaks Liskov substitution principle and the implementation that abides by it.

---
+++ SOLID-Principles/SOLID-Principles/Bird.cs
@@ -0,0 +1,47 @@
+﻿namespace SOLID_Principles
+{
+    /*
+     * This demonstrated the implementation where Liskov Substitution Principle is broken
+    public class Bird
+    {
+        public void Fly()
+        {
+        }
+    }
+
+    /// <summary>
+    /// We can see here that this does not follow Liskov Substitution Principle
+    /// If we substitute the subclass (Ostritch) with the base class (Bird). It does not work because an Ostritch does not fly
+    /// This implementation is where the Liskov Substitution Principle is broken
+    /// </summary>
+    public class Ostrich: Bird
+    {
+
+    }
+    */
+
+    /**
+     * We can see in the implementation below that if we substitute any of the subclasses with the base class the implementation still stands
+     * Pigeon can be substituted with FlyingBird
+     * Ostritch can be substitued with Bird
+    */
+    public class Bird
+    {
+
+    }
+    public class FlyingBird : Bird
+    {
+        public void Fly() { }
+    }
+
+    public class Ostrictch: Bird
+    {
+
+    }
+    public class Pigeon: FlyingBird
+    {
+
+    }
+
+
+}
